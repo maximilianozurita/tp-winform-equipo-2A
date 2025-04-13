278695e8debe2ddfa733717c41c9acc680d6639b
maximilianozurita <maxi.zurita.seb@gmail.com>
2025-04-13 09:58:26 -0300

    Primer concepto de clases de dominio

---
+++ app/TPWinForm_equipo-2A/dominio/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Articulo
+    {
+        public int ID { get; set; }
+        public string CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public string Imagen { get; set; }
+        public float Precio { get; set; }
+    }
+}
