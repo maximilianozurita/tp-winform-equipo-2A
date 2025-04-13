98d2f8ce167b4c7d59742bb583439bde9eab55c4
maximilianozurita <maxi.zurita.seb@gmail.com>
2025-04-13 00:29:45 -0300

    Se agrega clase AccesoDatos para conectar a DB

---
+++ app/TPWinForm_equipo-2A/negocio/AccesoDatos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        //ToDo: Ver si guardar en un db.config (depende de la DB de cada uno)
+        const string DB_CONFIG = "server=.//sql; database=CATALOGO_P3_DB; integrated secutiry=true";
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection(DB_CONFIG);
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+    }
+}
